e2c9162cc58980b377b5aba5ae13e4ab6d21e060
daveliu <dave@liageren.com>
2013-03-21 18:18:10 +0800

    Initial Commit

---
+++ RubyChina/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RubyChina
+//
+//  Created by dave on 3/21/13.
+//  Copyright (c) 2013 dave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
